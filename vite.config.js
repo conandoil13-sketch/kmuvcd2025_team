bf7a39ab919ab4e60b3124b45937abe3e029c0f5
임수호 <alltimesuho@imsuhoui-MacBookPro.local>
2025-10-26 15:57:46 +0900

    ha

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    // GitHub Pages용 경로 설정 (레포 이름과 완전히 동일해야 함)
-    base: '/kmuvcd2025_team/',
-
     server: {
         host: true,
         port: 5173,
         open: true,
         strictPort: true
-    },
-
-    build: {
-        outDir: 'dist', // 기본값, 명시적으로 지정
-        assetsDir: 'assets', // 정적 자산 폴더
     }
 });
