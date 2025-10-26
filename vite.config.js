69733306e1e45100dced4606a84186fbf42119e5
임수호 <alltimesuho@imsuhoui-MacBookPro.local>
2025-10-26 16:23:26 +0900

    allfix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    // ✅ 깃허브 레포 이름 그대로 써야 함!
+    base: '/kmuvcd2025_team/',
+
     server: {
         host: true,
         port: 5173,
         open: true,
-        strictPort: true
-    }
+        strictPort: true,
+    },
+
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+    },
 });
